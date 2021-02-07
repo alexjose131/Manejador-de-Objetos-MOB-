c078bf11537be8df59f0b39286b9da658eecd1b3
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2021-02-07 00:55:51 -0400

    se agrega base del controlador, y conexion por sockets MOB-controlador

--- MOB/controllers/objetoController.js
+++ MOB/controllers/objetoController.js
@@ -1,17 +1,18 @@
 import models from "../models";
 import fs from "fs";
 import parser from "xml2json";
+import { io } from "socket.io-client";
 
 export default {
   crear: async (req, res, next) => {
     try {
       console.log("req.body: ", req.body);
       let reg = [];
-      fs.readFile("statics/database.xml", function (err, data) {
+      fs.readFile(process.env.DATABASE_URL, function (err, data) {
         reg = JSON.parse(parser.toJson(data, { reversible: true }));
         reg.objetos.objeto.push(req.body);
         reg = parser.toXml((reg), {reversible: true});
-        fs.writeFile("statics/database.xml", reg, () => {});
+        fs.writeFile(process.env.DATABASE_URL, reg, () => {});
       });
 
       res.status(200).json({message: "Added succesfully."});
@@ -25,7 +26,7 @@ export default {
   consultar: async (req, res, next) => {
     try {
       let reg;
-      fs.readFile("statics/database.xml", function (err, data) {
+      fs.readFile(process.env.DATABASE_URL, function (err, data) {
         reg = JSON.parse(parser.toJson(data, { reversible: true })).objetos.objeto;
         res.status(200).json(reg);
       });
@@ -40,13 +41,13 @@ export default {
   eliminar: async (req, res, next) => {
     try {
       let reg = 0;
-      fs.readFile("statics/database.xml", function (err, data) {
+      fs.readFile(process.env.DATABASE_URL, function (err, data) {
          reg = JSON.parse(parser.toJson(data, { reversible: true }));
          reg.objetos.objeto.forEach((objeto, i) => {
            if (objeto.nombre.$t=== req.body.nombre.$t && objeto.fecha.$t === req.body.fecha.$t && objeto.accion.$t === req.body.accion.$t) {
             reg.objetos.objeto.splice(i,1);
             reg = parser.toXml((reg), {reversible: true});
-            fs.writeFile("statics/database.xml", reg, () => {});
+            fs.writeFile(process.env.DATABASE_URL, reg, () => {});
             res.status(200).json({message: "Deleted successfully"});
            }
          });
@@ -64,7 +65,13 @@ export default {
     try {
       console.log("req.body: ", req.body);
       const reg = 0;
-      // logica para replicar en XML
+      const socket = io("http://" + process.env.SERVER_CONTROLLER_IP + ":" + process.env.SERVER_CONTROLLER_PORT);
+      
+      socket.on("connect", () => {
+        socket.emit('new_message', 'this is a test');
+        socket.disconnect();
+      });
+
       res.status(200).json(reg);
     } catch (e) {
       res.status(500).send({
--- MOB/index.js
+++ MOB/index.js
@@ -4,9 +4,10 @@ import cors from "cors";
 import path from "path";
 import router from "./routes";
 import dotenv from "dotenv";
-dotenv.config();
 
+dotenv.config();
 const app = express();
+
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
@@ -15,7 +16,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", router);
 app.set("port", process.env.PORT || 3000);
 
+
 app.listen(app.get("port"), () => {
   console.log(`Server running in port ${app.get("port")}`);
   console.log(path.join(__dirname, "public"));
 });
+
